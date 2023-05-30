ce89d4e1cb782f94d495bbd9fdf5a64c8b47c366
alerommic <alerommic@users.noreply.github.com>
2023-05-30 21:26:02 +0000

    deploy: bbde41886172d65262ed72cbddb8b2340458858d

---
+++ assets/AboutView-bbe3efa5.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,a as o,_ as a}from"./index-d16dec82.js";const s={class:"about"},i={__name:"AboutView",setup(c){return(_,r)=>(e(),t("div",s,[o(a,{title:"holo"})]))}};export{i as default};
