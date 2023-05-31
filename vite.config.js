30ff8dccda52ce61d226fb345c3e4ea217d23ba0
Alex <122971295+alerommic@users.noreply.github.com>
2023-05-31 08:26:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/my_blog/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/vue-practicar/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
